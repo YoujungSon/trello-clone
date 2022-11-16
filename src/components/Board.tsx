18c22efbb916424c8c55f7d41362eaeb036f1daf
YoujungSon <andn1026@gmail.com>
2022-11-16 20:38:34 +0900

    #7.9 Same Board Movement

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,21 +9,32 @@ const Wrapper = styled.div`
   border-radius: 5px;
   min-height: 200px;
 `;
+
+const Title = styled.h2`
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+
 interface IBoardProps {
   toDos: string[];
   boardId: string;
 }
 function Board({ toDos, boardId }: IBoardProps) {
   return (
-    <Droppable droppableId={boardId}>
-      {(magic) => (
-        <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-          {toDos.map((toDo, index) => (
-            <DragabbleCard key={toDo} index={index} toDo={toDo} />
-          ))}
-        </Wrapper>
-      )}
-    </Droppable>
+    <Wrapper>
+      <Title>{boardId}</Title>
+      <Droppable droppableId={boardId}>
+        {(magic) => (
+          <div ref={magic.innerRef} {...magic.droppableProps}>
+            {toDos.map((toDo, index) => (
+              <DragabbleCard key={toDo} index={index} toDo={toDo} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </Wrapper>
   );
 }
 
